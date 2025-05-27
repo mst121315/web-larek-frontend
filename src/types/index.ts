033d3ffa36f15e0c965a68596048752dc29b12c1
Marina Sazonova <mst121315@gmail.com>
2025-05-27 22:20:32 +0200

    types i Readme2

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,38 +4,60 @@ interface IProduct {
     category: string;
     title: string;
     description: string;
-    price: number;
+    price: number | null;
 }
 
-class product implements IProduct {
-    id: string;
-    image: string;
-    category: string;
-    title: string;
-    description: string;
-    price: number;
-
-    constructor(id: string, image: string, category: string, title: string, description: string, price: number) {
-      this.id = id
-      this.image = image
-      this.category = category
-      this.title = title
-      this.description = description
-      this.price = price
-    }
+class Cart {
+  items: IProduct[];
+  subtotal(): number;
 }
 
 type PaymentMethod = 'cash' | 'card';
 
-interface IOrder {
-  items: product[];
+class Quote {
+  cart: Cart;
   payment: PaymentMethod;
   adress: string;
   email: string;
   phone: string;
 }
 
-interface Cart {
-    items: product[];
-    totalPrice: number;
+interface IOrder {
+  id: string;
+  total: number;
+}
+
+abstract class ModalView {
+  open(): void;
+  close(): void;
+}
+
+class ProductPreviewView extends ModalView {
+  render(IProduct): void
+  onBuyClick(): void
+}
+
+class PaymantAdressView extends ModalView {
+  onClickPaymentMethod(): void
+  isValid(): boolean
+  onClick: void
+}
+
+class ContactView extends ModalView {
+  isValid(): boolean
+  onClickPayment: void
+}
+
+class FinishView extends ModalView {
+  render(TotalPrice): number;
+  onClickFinish: void
+}
+
+class WebLarek extends API {
+  constructor(baseUrl: string) {
+  }
+
+  productList(): Product[]
+  productItem(id: string): Product
+  order(Quote): Order
 }
